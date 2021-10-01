267be1ba0b7748735e9d820a8c7918ec9c4639a4
Rhys Bridger <5bridr11@solent.ac.uk>
2021-10-01 15:22:38 +0100

    added code to display an ascii art robot

---
+++ basics/output/escape_chracters.py
@@ -0,0 +1,10 @@
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+
+print("\n")
+print ("\t I am beep" )
+print("\n")
