a634902b7393af246c6949ba5bbe77736a184013
Rhys Bridger <5bridr11@solent.ac.uk>
2021-10-08 15:11:57 +0100

    Added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,9 @@
+first_number=int(input("please enter your first number"))
+second_number=int(input("please enter your second number"))
+print()
+if first_number >second_number:
+    print("the first number is bigger!")
+    if first_number == second_number:
+        print("they are equal!")
+else:
+    print("the second number are bigger!")
--- basics/decisions/simple_decision/modulo_operator.py
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -1,1 +1,10 @@
-git
+#asking the user to enter a wholenumber
+number=int(input("please enter a whole number!"))
+print(number)
+print()
+for number in range(0):
+    if(number % 2 != 0):
+        print("this number is odd!")
+
+else :
+        print("this number is even")
