3068c80d33a2fbafb76fe6da636c9ed53647e9e6
Rhys Bridger <5bridr11@solent.ac.uk>
2021-10-08 15:40:34 +0100

    Added code to demonstrate how a variable can be used as a counter.

--- basics/decisions/simple_decision/comparison_operators.py
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -1,9 +1,10 @@
 first_number=int(input("please enter your first number"))
+print(first_number)
 second_number=int(input("please enter your second number"))
-print()
-if first_number >second_number:
+print(second_number)
+if first_number > second_number:
     print("the first number is bigger!")
-    if first_number == second_number:
+elif first_number == second_number:
         print("they are equal!")
 else:
     print("the second number are bigger!")
