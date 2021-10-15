ef6e4fba1c17a86b8a4fc5d51065fbcaf1035f73
Rhys Bridger <5bridr11@solent.ac.uk>
2021-10-15 14:48:59 +0100

    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,9 +1,8 @@
 
 print("program has started")
-character=(input("Please enter a standard character"))
+character=str(input("Please enter a standard character"))
 print(character)
-
-number = number + ord()
-if character==len(1):
-    number == ord(character)
-    print(f"the ascii code for {character} is {number}")
+if len(character) == 1:
+    number = ord(character)
+    print(f"the ascii code for {character} is {number}!")
+print ("program ended")
