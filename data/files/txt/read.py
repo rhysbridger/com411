a2e41514243981148abe1cab9b73599be20c12aa
Rhys Bridger <5bridr11@solent.ac.uk>
2021-10-29 14:14:10 +0100

    Added code to demonstrate a simple list.

---
+++ data/files/txt/read.py
@@ -0,0 +1,13 @@
+import library
+def display_chars():
+    def display_line():
+        def display_text():
+            def run():
+            print("The first 5 letters are:")
+
+            print("The first line is:")
+
+            print("The full text is:")
+for file in library
+if __name__ == "__main__":
+    run()
