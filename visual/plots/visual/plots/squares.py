7c66407f574a06a5ba54ae837ccb7c12fa5b413a
Rhys Bridger <5bridr11@solent.ac.uk>
2021-11-12 15:07:40 +0000

    Added code to display multiple plots.

---
+++ visual/plots/visual/plots/squares.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def small(x, y):
+    x_values = [3, 3, 4, 4]
+    y_values = [3, 3, 4, 4]
+    plt.plot(x, y)
+    plt.show()
+
+
+def medium(x, y):
+    x_values = [2, 5, 2, 5]
+    y_values = [2, 5, 2, 5]
+    plt.plot(x, y)
+    plt.show()
+
+def large(x, y):
+    x_values = [1, 1, 6, 6,]
+    y_values = [1, 1, 6, 6]
+    plt.plot(x, y)
+    plt.show()
+
+
+def run():
+
+
+
+run()
